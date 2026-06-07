6f67d23c4564318bb74fe9e335b8337043a206b4
HARSH PANDEY <harshpandey.vlsi@gmail.com>
2026-06-08 00:12:02 +0530

    Create scoreboard.sv

---
+++ tb/scoreboard.sv
@@ -0,0 +1,142 @@
+class apb_scoreboard;
+
+   mailbox #(apb_transaction) mon2sb;
+
+  bit [31:0] ref_mem[256];
+
+   int total_checks;
+   int pass_count;
+   int fail_count;
+
+   function new(mailbox #(apb_transaction) mb);
+
+      mon2sb = mb;
+
+      total_checks = 0;
+      pass_count   = 0;
+      fail_count   = 0;
+
+   endfunction
+
+   task run();
+
+      apb_transaction tr;
+
+      forever begin
+
+         mon2sb.get(tr);
+
+         if(tr.write) begin
+
+            ref_mem[tr.addr] = tr.data;
+
+            $display("[SB] WRITE ADDR=%0h DATA=%0h",
+                     tr.addr,
+                     tr.data);
+
+         end
+         else begin
+
+            total_checks++;
+
+            if(tr.rdata === ref_mem[tr.addr]) begin
+
+               pass_count++;
+
+               $display(
+               "[SB PASS] ADDR=%0h EXP=%0h GOT=%0h",
+               tr.addr,
+               ref_mem[tr.addr],
+               tr.rdata);
+
+            end
+            else begin
+
+               fail_count++;
+
+               $error(
+               "[SB FAIL] ADDR=%0h EXP=%0h GOT=%0h",
+               tr.addr,
+               ref_mem[tr.addr],
+               tr.rdata);
+
+            end
+
+         end
+
+      end
+
+   endtask
+
+   function void report();
+
+      $display("\n================================");
+      $display("      SCOREBOARD REPORT");
+      $display("================================");
+      $display("TOTAL CHECKS : %0d", total_checks);
+      $display("PASS COUNT   : %0d", pass_count);
+      $display("FAIL COUNT   : %0d", fail_count);
+      $display("================================\n");
+
+   endfunction
+
+endclass
+
+class apb_coverage;
+
+   virtual apb_if vif;
+
+   covergroup apb_cg @(posedge vif.PCLK);
+
+      cp_write :
+      coverpoint vif.PWRITE
+      {
+         bins read  = {0};
+         bins write = {1};
+      }
+
+      cp_addr :
+      coverpoint vif.PADDR
+      {
+         bins low  = {[0:63]};
+         bins mid  = {[64:127]};
+         bins high = {[128:255]};
+      }
+
+      cp_error :
+      coverpoint vif.PSLVERR
+      {
+         bins no_error = {0};
+         bins error    = {1};
+      }
+
+      cross cp_write,
+            cp_addr;
+
+   endgroup
+  
+
+   function new(virtual apb_if vif);
+
+      this.vif = vif;
+
+      apb_cg = new();
+
+   endfunction
+
+   function void report();
+
+      real cov;
+
+      cov = apb_cg.get_inst_coverage();
+     
+
+      $display("\n================================");
+      $display(" FUNCTIONAL COVERAGE REPORT");
+      $display("================================");
+      $display("Coverage = %0.2f %%", cov);
+      $display("================================\n");
+
+   endfunction
+
+endclass
