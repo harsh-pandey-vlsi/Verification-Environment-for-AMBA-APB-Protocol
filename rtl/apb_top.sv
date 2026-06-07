68502bbd3feaa4c29caf4655681d585ad2f7cb2e
HARSH PANDEY <harshpandey.vlsi@gmail.com>
2026-06-08 00:07:22 +0530

    Update apb_top.sv

--- rtl/apb_top.sv
+++ rtl/apb_top.sv
@@ -1,41 +1,84 @@
 module apb_top #(
-  parameter ADDR_WIDTH=8,
-  parameter DATA_WIDTH=32,
-  parameter DEPTH=256,
-  parameter WAIT_CYCLES=2
-  )
-  (
-  input logic PCLK,PRESETn,
-  input logic start,write,
-  input logic [ADDR_WIDTH-1:0]addr,
-  input logic [DATA_WIDTH-1:0]wdata,
-  output logic [DATA_WIDTH-1:0]rdata,
-  output logic done,
-  output logic error
-);
-  logic PSEL,PENABLE,PWRITE;
-  logic [ADDR_WIDTH-1:0]PADDR;
-  logic [DATA_WIDTH-1:0]PWDATA,PRDATA;
-  logic PREAD,PSLVERR;
+    parameter ADDR_WIDTH = 8,
+    parameter DATA_WIDTH = 32,
+    parameter DEPTH = 256,
+    parameter WAIT_CYCLES = 2
+)(
+    input  logic                    PCLK,
+    input  logic                    PRESETn,
+
+    input  logic                    start,
+    input  logic                    write,
+    input  logic [ADDR_WIDTH-1:0]   addr,
+    input  logic [DATA_WIDTH-1:0]   wdata,
+
+    output logic [DATA_WIDTH-1:0]   rdata,
+    output logic                    done,
+    output logic                    error,
   
-  apb_master #(
+   output logic PSEL,
+   output logic PENABLE,
+   output logic PWRITE,
+
+   output logic [7:0] PADDR,
+   output logic [31:0] PWDATA,
+
+   output logic [31:0] PRDATA,
+   output logic PREADY,
+   output logic PSLVERR
+);
+
+
+
+apb_master #(
     .ADDR_WIDTH(ADDR_WIDTH),
     .DATA_WIDTH(DATA_WIDTH)
-  ) master (
-    .*
-  );
-  
-  apb_slave #(
+) u_master (
+
+    .PCLK(PCLK),
+    .PRESETn(PRESETn),
+
+    .PSEL(PSEL),
+    .PENABLE(PENABLE),
+    .PWRITE(PWRITE),
+
+    .PADDR(PADDR),
+    .PWDATA(PWDATA),
+
+    .PRDATA(PRDATA),
+    .PREADY(PREADY),
+    .PSLVERR(PSLVERR),
+
+    .start(start),
+    .write(write),
+    .addr(addr),
+    .wdata(wdata),
+
+    .rdata(rdata),
+    .done(done),
+    .error(error)
+);
+
+apb_slave #(
     .ADDR_WIDTH(ADDR_WIDTH),
     .DATA_WIDTH(DATA_WIDTH),
     .DEPTH(DEPTH),
     .WAIT_CYCLES(WAIT_CYCLES)
-  ) slave (
-    .PCLK(PCLK), .PRESETn(PRESETn),
-    .PSEL(PSEL), .PENABLE(PENABLE),
-    .PWRITE(PWRITE), .PADDR(PADDR),
-    .PWDATA(PWDATA), .PRDATA(PRDATA),
-    .PREADY(PREADY), .PSLVERR(PSLVERR)
-  );
-  
+) u_slave (
+
+    .PCLK(PCLK),
+    .PRESETn(PRESETn),
+
+    .PSEL(PSEL),
+    .PENABLE(PENABLE),
+    .PWRITE(PWRITE),
+
+    .PADDR(PADDR),
+    .PWDATA(PWDATA),
+
+    .PRDATA(PRDATA),
+    .PREADY(PREADY),
+    .PSLVERR(PSLVERR)
+);
+
 endmodule
